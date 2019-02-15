3517512c4a6fa7eb61b8c5e5eca38c52c5577631
crupest <crupest@outlook.com>
2019-02-15 16:08:08 +0800

    Develop front end.

--- Timeline/ClientApp/src/app/app.component.ts
+++ Timeline/ClientApp/src/app/app.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  public isCollapse = false;
 }
--- Timeline/ClientApp/src/app/app.module.ts
+++ Timeline/ClientApp/src/app/app.module.ts
@@ -3,21 +3,28 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatMenuModule, MatIconModule, MatButtonModule, MatToolbarModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { TodoListPageComponent } from './todo-list-page/todo-list-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    TodoListPageComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
+    BrowserAnimationsModule,
+    MatMenuModule, MatIconModule, MatButtonModule, MatToolbarModule,
     RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' }
+      { path: '', component: HomeComponent, pathMatch: 'full' },
+      { path: 'todo', component: TodoListPageComponent}
     ])
   ],
   providers: [],
