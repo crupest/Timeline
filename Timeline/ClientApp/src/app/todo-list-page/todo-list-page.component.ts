f867fb7fed885c474dda18e9a9ca7b1d64c9407f
crupest <crupest@outlook.com>
2019-02-15 16:08:08 +0800

    Develop front end.

---
+++ Timeline/ClientApp/src/app/todo-list-page/todo-list-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-list-page',
+  templateUrl: './todo-list-page.component.html',
+  styleUrls: ['./todo-list-page.component.css']
+})
+export class TodoListPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
