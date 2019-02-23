dd33a3df1027e3aa5f89935c54749c3ace68eea7
crupest <crupest@outlook.com>
2019-02-24 00:14:59 +0800

    Update unit tests.

--- Timeline/ClientApp/src/app/todo-item/todo-item.component.spec.ts
+++ Timeline/ClientApp/src/app/todo-item/todo-item.component.spec.ts
@@ -1,25 +1,53 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TodoItemComponent } from './todo-item.component';
+import { WorkItem } from '../todo-list-page/todo-list.service';
+import { By } from '@angular/platform-browser';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 describe('TodoItemComponent', () => {
   let component: TodoItemComponent;
   let fixture: ComponentFixture<TodoItemComponent>;
 
+  let mockWorkItem: WorkItem;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TodoItemComponent ]
-    })
-    .compileComponents();
+      declarations: [TodoItemComponent],
+      schemas: [NO_ERRORS_SCHEMA]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
+    mockWorkItem = {
+      id: 0,
+      title: 'Title',
+      closed: true,
+      detailUrl: '/detail',
+      iconUrl: '/icon'
+    };
+
     fixture = TestBed.createComponent(TodoItemComponent);
     component = fixture.componentInstance;
+    component.item = mockWorkItem;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should set icon', () => {
+    expect(fixture.debugElement.query(By.css('img.item-icon')).properties['src']).toBe(mockWorkItem.iconUrl);
+  });
+
+  it('should set title', () => {
+    expect((fixture.debugElement.query(By.css('span.item-title')).nativeElement as HTMLSpanElement).textContent).toBe(
+      mockWorkItem.id + '. ' + mockWorkItem.title
+    );
+  });
+
+  it('should set detail link', () => {
+    expect(fixture.debugElement.query(By.css('a.item-detail-button')).properties['href']).toBe(mockWorkItem.detailUrl);
   });
 });
