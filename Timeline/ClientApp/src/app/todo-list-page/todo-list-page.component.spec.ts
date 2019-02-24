764c499add501af742b7098f4cc204feadbccda4
crupest <crupest@outlook.com>
2019-02-25 00:40:27 +0800

    Add entrance animation of todo item.

--- Timeline/ClientApp/src/app/todo-list-page/todo-list-page.component.spec.ts
+++ Timeline/ClientApp/src/app/todo-list-page/todo-list-page.component.spec.ts
@@ -1,23 +1,23 @@
 import { Component, NO_ERRORS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
 
-import { defer, Observable } from 'rxjs';
+import { Observable, from } from 'rxjs';
 
 import { TodoListPageComponent } from './todo-list-page.component';
 import { TodoListService, WorkItem } from './todo-list.service';
 import { By } from '@angular/platform-browser';
+import { delay } from 'rxjs/operators';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 @Component({
   /* tslint:disable-next-line:component-selector*/
   selector: 'mat-progress-bar',
   template: ''
 })
-class MatProgressBarStubComponent {
+class MatProgressBarStubComponent {}
 
-}
-
-function asyncData<T>(data: T): Observable<T> {
-  return defer(() => Promise.resolve(data));
+function asyncArray<T>(data: T[]): Observable<T> {
+  return from(data).pipe(delay(0));
 }
 
 describe('TodoListPageComponent', () => {
@@ -29,22 +29,31 @@ describe('TodoListPageComponent', () => {
   beforeEach(async(() => {
     const todoListService: jasmine.SpyObj<TodoListService> = jasmine.createSpyObj('TodoListService', ['getWorkItemList']);
 
-    mockWorkItems = [{
-      id: 0, title: 'Test title 1', isCompleted: true, detailUrl: 'https://test.org/workitems/0', iconUrl: 'https://test.org/icon/0'
-    }, {
-      id: 1, title: 'Test title 2', isCompleted: false, detailUrl: 'https://test.org/workitems/1', iconUrl: 'https://test.org/icon/1'
-    }];
+    mockWorkItems = [
+      {
+        id: 0,
+        title: 'Test title 1',
+        isCompleted: true,
+        detailUrl: 'https://test.org/workitems/0',
+        iconUrl: 'https://test.org/icon/0'
+      },
+      {
+        id: 1,
+        title: 'Test title 2',
+        isCompleted: false,
+        detailUrl: 'https://test.org/workitems/1',
+        iconUrl: 'https://test.org/icon/1'
+      }
+    ];
 
-    todoListService.getWorkItemList.and.returnValue(asyncData(mockWorkItems));
+    todoListService.getWorkItemList.and.returnValue(asyncArray(mockWorkItems));
 
     TestBed.configureTestingModule({
       declarations: [TodoListPageComponent, MatProgressBarStubComponent],
-      providers: [
-        { provide: TodoListService, useValue: todoListService }
-      ],
+      imports: [NoopAnimationsModule],
+      providers: [{ provide: TodoListService, useValue: todoListService }],
       schemas: [NO_ERRORS_SCHEMA]
-    })
-      .compileComponents();
+    }).compileComponents();
   }));
 
   beforeEach(() => {
