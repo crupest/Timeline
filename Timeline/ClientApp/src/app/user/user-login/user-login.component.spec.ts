2ee5c455152a0553453e400b387109b0b518ec99
crupest <crupest@outlook.com>
2019-03-06 23:14:45 +0800

    Write all unit tests.

--- Timeline/ClientApp/src/app/user/user-login/user-login.component.spec.ts
+++ Timeline/ClientApp/src/app/user/user-login/user-login.component.spec.ts
@@ -1,6 +1,9 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { async, ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
 
-import { UserLoginComponent } from './user-login.component';
+import { UserLoginComponent, LoginEvent } from './user-login.component';
 
 describe('UserLoginComponent', () => {
   let component: UserLoginComponent;
@@ -8,9 +11,11 @@ describe('UserLoginComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ UserLoginComponent ]
+      declarations: [UserLoginComponent],
+      imports: [ReactiveFormsModule],
+      schemas: [NO_ERRORS_SCHEMA]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -22,4 +27,34 @@ describe('UserLoginComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('reactive form should work well', () => {
+    const usernameInput = fixture.debugElement.query(By.css('input[type=text]')).nativeElement as HTMLInputElement;
+    const passwordInput = fixture.debugElement.query(By.css('input[type=password]')).nativeElement as HTMLInputElement;
+
+    usernameInput.value = 'user';
+    usernameInput.dispatchEvent(new Event('input'));
+    passwordInput.value = 'user';
+    passwordInput.dispatchEvent(new Event('input'));
+
+    fixture.detectChanges();
+
+    expect(component.form.value).toEqual({
+      username: 'user',
+      password: 'user'
+    });
+  });
+
+  it('login event should work well', fakeAsync(() => {
+    let userCredential: LoginEvent;
+    component.login.subscribe((e: LoginEvent) => { userCredential = e; });
+    fixture.detectChanges();
+    const mockValue = {
+      username: 'user',
+      password: 'user'
+    };
+    component.form.setValue(mockValue);
+    component.onLoginButtonClick();
+    expect(userCredential).toEqual(mockValue);
+  }));
 });
