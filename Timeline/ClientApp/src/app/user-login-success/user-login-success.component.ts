8ec742c4a00c1732d6aa58a1cb8b628428d23c4a
crupest <crupest@outlook.com>
2019-03-06 21:12:59 +0800

    Extract out user-login-success component.

---
+++ Timeline/ClientApp/src/app/user-login-success/user-login-success.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { UserInfo } from '../user-dialog/user.service';
+
+@Component({
+  selector: 'app-user-login-success',
+  templateUrl: './user-login-success.component.html',
+  styleUrls: ['./user-login-success.component.css']
+})
+export class UserLoginSuccessComponent implements OnInit {
+
+  @Input()
+  displayLoginSuccessMessage = false;
+
+  @Input()
+  userInfo: UserInfo;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
