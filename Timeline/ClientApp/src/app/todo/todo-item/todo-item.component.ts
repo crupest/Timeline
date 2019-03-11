87f7b00f9414b7633a080838a725a687461efd68
crupest <crupest@outlook.com>
2019-03-11 21:21:02 +0800

    Fix some compile bugs.

--- Timeline/ClientApp/src/app/todo/todo-item/todo-item.component.ts
+++ Timeline/ClientApp/src/app/todo/todo-item/todo-item.component.ts
@@ -1,18 +1,12 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 import { TodoItem } from '../todo-item';
-import { throwIfFalsy } from 'src/app/utilities/language-untilities';
 
 @Component({
   selector: 'app-todo-item',
   templateUrl: './todo-item.component.html',
   styleUrls: ['./todo-item.component.css', '../todo-list-color-block.css']
 })
-export class TodoItemComponent implements OnInit {
-
-  @Input() item: TodoItem | undefined;
-
-  ngOnInit() {
-    throwIfFalsy(this.item, 'item');
-  }
+export class TodoItemComponent {
+  @Input() item!: TodoItem;
 }
--- Timeline/ClientApp/src/app/utilities/language-untilities.ts
+++ Timeline/ClientApp/src/app/utilities/language-untilities.ts
@@ -2,8 +2,11 @@ export function nullIfUndefined<T>(value: T | undefined): T | null {
   return value === undefined ? null : value;
 }
 
-export function throwIfFalsy(value: any, name: string = '<unknown name>') {
-  if (!value) {
-    throw new Error(name + ' is falsy.');
+export function throwIfNullOrUndefined<T>(value: T | null | undefined,
+  lazyMessage: () => string = () => 'Value mustn\'t be falsy'): T | never {
+  if (value === null || value === undefined) {
+    throw new Error(lazyMessage());
+  } else {
+    return value;
   }
 }
