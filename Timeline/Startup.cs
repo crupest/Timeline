259570710123d000831dc8f3c24d8cd78ed66320
crupest <crupest@outlook.com>
2019-04-13 13:11:51 +0800

    add a using

--- Timeline/Startup.cs
+++ Timeline/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
