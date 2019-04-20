a9f248ad817683e911348cd168c570db3d07757f
crupest <crupest@outlook.com>
2019-04-21 00:08:59 +0800

    Reorgnize api. Add basic unit test.

--- Timeline.Tests/Helpers/Authentication/AuthenticationExtensions.cs
+++ Timeline.Tests/Helpers/Authentication/AuthenticationExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json;
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Newtonsoft.Json;
 using System;
 using System.Net;
 using System.Net.Http;
@@ -8,9 +9,9 @@ using Xunit;
 
 namespace Timeline.Tests.Helpers.Authentication
 {
-    public static class AuthenticationHttpClientExtensions
+    public static class AuthenticationExtensions
     {
-        private const string CreateTokenUrl = "/User/CreateToken";
+        private const string CreateTokenUrl = "/token/create";
 
         public static async Task<CreateTokenResponse> CreateUserTokenAsync(this HttpClient client, string username, string password, bool assertSuccess = true)
         {
@@ -22,6 +23,14 @@ namespace Timeline.Tests.Helpers.Authentication
                 Assert.True(result.Success);
 
             return result;
+        }
+
+        public static async Task<HttpClient> CreateClientWithUser<T>(this WebApplicationFactory<T> factory, string username, string password) where T : class
+        {
+            var client = factory.CreateDefaultClient();
+            var token = (await client.CreateUserTokenAsync(username, password)).Token;
+            client.DefaultRequestHeaders.Add("Authorization", "Bearer " + token);
+            return client;
         }
 
         public static async Task<HttpResponseMessage> SendWithAuthenticationAsync(this HttpClient client, string token, string path, Action<HttpRequestMessage> requestBuilder = null)
--- Timeline/Entities/UserInfo.cs
+++ Timeline/Entities/UserInfo.cs
@@ -1,14 +1,20 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using Timeline.Models;
 
 namespace Timeline.Entities
 {
-    public class UserInfo
+    public sealed class UserInfo
     {
         public UserInfo()
         {
+        }
 
+        public UserInfo(string username, params string[] roles)
+        {
+            Username = username;
+            Roles = roles;
         }
 
         public UserInfo(User user)
@@ -17,10 +23,64 @@ namespace Timeline.Entities
                 throw new ArgumentNullException(nameof(user));
 
             Username = user.Name;
-            Roles = user.RoleString.Split(',').Select(s => s.Trim()).ToArray();
+
+            if (user.RoleString == null)
+                Roles = null;
+            else
+                Roles = user.RoleString.Split(',').Select(r => r.Trim()).ToArray();
         }
 
         public string Username { get; set; }
         public string[] Roles { get; set; }
+
+        public static IEqualityComparer<UserInfo> EqualityComparer { get; } = new EqualityComparerImpl();
+        public static IComparer<UserInfo> Comparer { get; } = Comparer<UserInfo>.Create(Compare);
+
+        private class EqualityComparerImpl : IEqualityComparer<UserInfo>
+        {
+            bool IEqualityComparer<UserInfo>.Equals(UserInfo x, UserInfo y)
+            {
+                return Compare(x, y) == 0;
+            }
+
+            int IEqualityComparer<UserInfo>.GetHashCode(UserInfo obj)
+            {
+                return obj.Username.GetHashCode() ^ NormalizeRoles(obj.Roles).GetHashCode();
+            }
+        }
+
+        private static string NormalizeRoles(string[] rawRoles)
+        {
+            var roles = rawRoles.Where(r => !string.IsNullOrWhiteSpace(r)).Select(r => r.Trim()).ToList();
+            roles.Sort();
+            return string.Join(',', roles);
+        }
+
+        public static int Compare(UserInfo left, UserInfo right)
+        {
+            if (left == null)
+            {
+                if (right == null)
+                    return 0;
+                return -1;
+            }
+
+            if (right == null)
+                return 1;
+
+            var uc = string.Compare(left.Username, right.Username);
+            if (uc != 0)
+                return uc;
+
+            var leftRoles = NormalizeRoles(left.Roles);
+            var rightRoles = NormalizeRoles(right.Roles);
+
+            return string.Compare(leftRoles, rightRoles);
+        }
+
+        public override string ToString()
+        {
+            return $"Username: {Username} ; Roles: {Roles}";
+        }
     }
 }
