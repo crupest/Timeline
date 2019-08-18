9a674f6f3a15ef260868049d945d7afc930f093c
杨宇千 <crupest@outlook.com>
2019-08-19 01:44:29 +0800

    Add integrated tests.

---
+++ Timeline.Tests/Helpers/ImageHelper.cs
@@ -0,0 +1,21 @@
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using System.IO;
+
+namespace Timeline.Tests.Helpers
+{
+    public static class ImageHelper
+    {
+        public static byte[] CreatePngWithSize(int width, int height)
+        {
+            using (var image = new Image<Rgba32>(width, height))
+            {
+                using (var stream = new MemoryStream())
+                {
+                    image.SaveAsPng(stream);
+                    return stream.ToArray();
+                }
+            }
+        }
+    }
+}
