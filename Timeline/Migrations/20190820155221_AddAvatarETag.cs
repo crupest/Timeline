80f0e72f000e195ae3778e70b88dfa793486abbd
杨宇千 <crupest@outlook.com>
2019-08-20 23:54:22 +0800

    Update Database.

---
+++ Timeline/Migrations/20190820155221_AddAvatarETag.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Timeline.Migrations
+{
+    public partial class AddAvatarETag : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "etag",
+                table: "user_avatars",
+                maxLength: 30,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "etag",
+                table: "user_avatars");
+        }
+    }
+}
