50924d4404bb8b46ef051123cd9fd851bc08c844
杨宇千 <crupest@outlook.com>
2019-08-22 15:25:58 +0800

    Update database.

---
+++ Timeline/Migrations/20190822072156_AddUserDetail.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Timeline.Migrations
+{
+    public partial class AddUserDetail : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "user_details",
+                columns: table => new
+                {
+                    id = table.Column<long>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    nickname = table.Column<string>(maxLength: 15, nullable: true),
+                    qq = table.Column<string>(maxLength: 15, nullable: true),
+                    email = table.Column<string>(maxLength: 50, nullable: true),
+                    phone_number = table.Column<string>(maxLength: 15, nullable: true),
+                    description = table.Column<string>(nullable: true),
+                    UserId = table.Column<long>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_user_details", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_user_details_users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_user_details_UserId",
+                table: "user_details",
+                column: "UserId",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "user_details");
+        }
+    }
+}
