c297fcdcee883b9a83019f733d9e7d51e66150b8
杨宇千 <crupest@outlook.com>
2019-10-31 14:58:36 +0800

    Recreate database and migrations.

---
+++ Timeline.Tests/PasswordGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Timeline.Services;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Timeline.Tests
+{
+    public class PasswordGenerator
+    {
+        private readonly ITestOutputHelper _output;
+
+        public PasswordGenerator(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [Fact]
+        public void Generate()
+        {
+            var service = new PasswordService();
+            _output.WriteLine(service.HashPassword("crupest"));
+        }
+    }
+}
