7acf0f674f1e3072b458721255a0bca12c629323
杨宇千 <crupest@outlook.com>
2019-11-04 22:58:24 +0800

    Add controller primarily and of course redesign the service accordingly.

--- Timeline/Entities/TimelineEntity.cs
+++ Timeline/Entities/TimelineEntity.cs
@@ -7,7 +7,17 @@ namespace Timeline.Entities
 {
     public enum TimelineVisibility
     {
+        /// <summary>
+        /// All people including those without accounts.
+        /// </summary>
         Public,
+        /// <summary>
+        /// Only people signed in.
+        /// </summary>
+        Register,
+        /// <summary>
+        /// Only member.
+        /// </summary>
         Private
     }
 
---
+++ Timeline/Models/Http/Timeline.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Timeline.Models.Http
+{
+    public class TimelinePostCreateRequest
+    {
+        [Required(AllowEmptyStrings = false)]
+        public string Content { get; set; } = default!;
+
+        public DateTime? Time { get; set; }
+    }
+
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "CA2227:Collection properties should be read only", Justification = "This is a DTO class.")]
+    public class TimelineMemberChangeRequest
+    {
+        public List<string>? Add { get; set; }
+
+        public List<string>? Remove { get; set; }
+    }
+}
