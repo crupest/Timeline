5ed6c3d191d3879a87ae0995ce72f3327b03aa52
crupest <crupest@outlook.com>
2019-11-20 18:48:14 +0800

    Update packages and migrate database.

--- Timeline/Migrations/DatabaseContextModelSnapshot.cs
+++ Timeline/Migrations/DatabaseContextModelSnapshot.cs
@@ -14,8 +14,102 @@ namespace Timeline.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "3.0.0")
+                .HasAnnotation("ProductVersion", "3.0.1")
                 .HasAnnotation("Relational:MaxIdentifierLength", 64);
+
+            modelBuilder.Entity("Timeline.Entities.TimelineEntity", b =>
+                {
+                    b.Property<long>("Id")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnName("id")
+                        .HasColumnType("bigint");
+
+                    b.Property<DateTime>("CreateTime")
+                        .HasColumnName("create_time")
+                        .HasColumnType("datetime(6)");
+
+                    b.Property<string>("Description")
+                        .HasColumnName("description")
+                        .HasColumnType("longtext CHARACTER SET utf8mb4");
+
+                    b.Property<string>("Name")
+                        .HasColumnName("name")
+                        .HasColumnType("longtext CHARACTER SET utf8mb4");
+
+                    b.Property<long>("OwnerId")
+                        .HasColumnName("owner")
+                        .HasColumnType("bigint");
+
+                    b.Property<int>("Visibility")
+                        .HasColumnName("visibility")
+                        .HasColumnType("int");
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("OwnerId");
+
+                    b.ToTable("timelines");
+                });
+
+            modelBuilder.Entity("Timeline.Entities.TimelineMemberEntity", b =>
+                {
+                    b.Property<long>("Id")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnName("id")
+                        .HasColumnType("bigint");
+
+                    b.Property<long>("TimelineId")
+                        .HasColumnName("timeline")
+                        .HasColumnType("bigint");
+
+                    b.Property<long>("UserId")
+                        .HasColumnName("user")
+                        .HasColumnType("bigint");
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("TimelineId");
+
+                    b.HasIndex("UserId");
+
+                    b.ToTable("TimelineMembers");
+                });
+
+            modelBuilder.Entity("Timeline.Entities.TimelinePostEntity", b =>
+                {
+                    b.Property<long>("Id")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnName("id")
+                        .HasColumnType("bigint");
+
+                    b.Property<long>("AuthorId")
+                        .HasColumnName("author")
+                        .HasColumnType("bigint");
+
+                    b.Property<string>("Content")
+                        .HasColumnName("content")
+                        .HasColumnType("longtext CHARACTER SET utf8mb4");
+
+                    b.Property<DateTime>("LastUpdated")
+                        .HasColumnName("last_updated")
+                        .HasColumnType("datetime(6)");
+
+                    b.Property<DateTime>("Time")
+                        .HasColumnName("time")
+                        .HasColumnType("datetime(6)");
+
+                    b.Property<long>("TimelineId")
+                        .HasColumnName("timeline")
+                        .HasColumnType("bigint");
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("AuthorId");
+
+                    b.HasIndex("TimelineId");
+
+                    b.ToTable("timeline_posts");
+                });
 
             modelBuilder.Entity("Timeline.Entities.User", b =>
                 {
@@ -27,18 +121,18 @@ namespace Timeline.Migrations
                     b.Property<string>("EncryptedPassword")
                         .IsRequired()
                         .HasColumnName("password")
-                        .HasColumnType("longtext");
+                        .HasColumnType("longtext CHARACTER SET utf8mb4");
 
                     b.Property<string>("Name")
                         .IsRequired()
                         .HasColumnName("name")
-                        .HasColumnType("varchar(26)")
+                        .HasColumnType("varchar(26) CHARACTER SET utf8mb4")
                         .HasMaxLength(26);
 
                     b.Property<string>("RoleString")
                         .IsRequired()
                         .HasColumnName("roles")
-                        .HasColumnType("longtext");
+                        .HasColumnType("longtext CHARACTER SET utf8mb4");
 
                     b.Property<long>("Version")
                         .ValueGeneratedOnAdd()
@@ -67,7 +161,7 @@ namespace Timeline.Migrations
 
                     b.Property<string>("ETag")
                         .HasColumnName("etag")
-                        .HasColumnType("varchar(30)")
+                        .HasColumnType("varchar(30) CHARACTER SET utf8mb4")
                         .HasMaxLength(30);
 
                     b.Property<DateTime>("LastModified")
@@ -76,7 +170,7 @@ namespace Timeline.Migrations
 
                     b.Property<string>("Type")
                         .HasColumnName("type")
-                        .HasColumnType("longtext");
+                        .HasColumnType("longtext CHARACTER SET utf8mb4");
 
                     b.Property<long>("UserId")
                         .HasColumnType("bigint");
@@ -98,7 +192,7 @@ namespace Timeline.Migrations
 
                     b.Property<string>("Nickname")
                         .HasColumnName("nickname")
-                        .HasColumnType("varchar(26)")
+                        .HasColumnType("varchar(26) CHARACTER SET utf8mb4")
                         .HasMaxLength(26);
 
                     b.Property<long>("UserId")
@@ -110,6 +204,45 @@ namespace Timeline.Migrations
                         .IsUnique();
 
                     b.ToTable("user_details");
+                });
+
+            modelBuilder.Entity("Timeline.Entities.TimelineEntity", b =>
+                {
+                    b.HasOne("Timeline.Entities.User", "Owner")
+                        .WithMany("Timelines")
+                        .HasForeignKey("OwnerId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+                });
+
+            modelBuilder.Entity("Timeline.Entities.TimelineMemberEntity", b =>
+                {
+                    b.HasOne("Timeline.Entities.TimelineEntity", "Timeline")
+                        .WithMany("Members")
+                        .HasForeignKey("TimelineId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+
+                    b.HasOne("Timeline.Entities.User", "User")
+                        .WithMany("TimelinesJoined")
+                        .HasForeignKey("UserId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+                });
+
+            modelBuilder.Entity("Timeline.Entities.TimelinePostEntity", b =>
+                {
+                    b.HasOne("Timeline.Entities.User", "Author")
+                        .WithMany("TimelinePosts")
+                        .HasForeignKey("AuthorId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+
+                    b.HasOne("Timeline.Entities.TimelineEntity", "Timeline")
+                        .WithMany("Posts")
+                        .HasForeignKey("TimelineId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
                 });
 
             modelBuilder.Entity("Timeline.Entities.UserAvatar", b =>
