eb43e9fba51d50aee59bf4c47423ef5c3d155e31
crupest <crupest@outlook.com>
2020-01-23 21:21:32 +0800

    Add some unit tests for token manager.

---
+++ Timeline.Tests/Services/UserTokenManagerTest.cs
@@ -0,0 +1,52 @@
+﻿using FluentAssertions;
+using Microsoft.Extensions.Logging.Abstractions;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Timeline.Services;
+using Timeline.Tests.Helpers;
+using Xunit;
+
+namespace Timeline.Tests.Services
+{
+    public class UserTokenManagerTest
+    {
+        private readonly UserTokenManager _service;
+
+        private readonly Mock<IUserService> _mockUserService;
+        private readonly Mock<IUserTokenService> _mockUserTokenService;
+        private readonly TestClock _mockClock;
+
+        public UserTokenManagerTest()
+        {
+            _mockUserService = new Mock<IUserService>();
+            _mockUserTokenService = new Mock<IUserTokenService>();
+            _mockClock = new TestClock();
+
+            _service = new UserTokenManager(NullLogger<UserTokenManager>.Instance, _mockUserService.Object, _mockUserTokenService.Object, _mockClock);
+        }
+
+        [Theory]
+        [InlineData(null, "aaa", "username")]
+        [InlineData("aaa", null, "password")]
+        public void CreateToken_NullArgument(string username, string password, string paramName)
+        {
+            _service.Invoking(s => s.CreateToken(username, password)).Should().Throw<ArgumentNullException>()
+                .Which.ParamName.Should().Be(paramName);
+        }
+
+        [Theory]
+        [InlineData(typeof(UsernameBadFormatException))]
+        [InlineData(typeof(UserNotExistException))]
+        [InlineData(typeof(BadPasswordException))]
+        public async Task CreateToken_VerifyCredential_Throw(Type exceptionType)
+        {
+            const string username = "uuu";
+            const string password = "ppp";
+            _mockUserService.Setup(s => s.VerifyCredential(username, password)).ThrowsAsync((Exception)Activator.CreateInstance(exceptionType));
+            await _service.Awaiting(s => s.CreateToken(username, password)).Should().ThrowAsync(exceptionType);
+        }
+    }
+}
