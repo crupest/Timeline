054b6237b902c7f7be08afe1229ab9cd84b99469
crupest <crupest@outlook.com>
2020-03-12 19:44:17 +0800

    Add database migration.

---
+++ Timeline/Migrations/20200312112552_AddImagePost.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Timeline.Models;
+
+namespace Timeline.Migrations
+{
+    public partial class AddImagePost : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "content_type",
+                table: "timeline_posts",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "extra_content",
+                table: "timeline_posts",
+                nullable: true);
+
+            migrationBuilder.Sql($@"
+UPDATE timeline_posts
+SET content_type = '{TimelinePostContentTypes.Text}';
+            ");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "content_type",
+                table: "timeline_posts");
+
+            migrationBuilder.DropColumn(
+                name: "extra_content",
+                table: "timeline_posts");
+        }
+    }
+}
