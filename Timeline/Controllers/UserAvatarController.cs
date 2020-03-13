8a1ecbf49673cb2bed538ac8bc4e82691b90d973
crupest <crupest@outlook.com>
2020-03-13 17:05:03 +0800

    Abstract out data cache helper.

--- Timeline/Controllers/UserAvatarController.cs
+++ Timeline/Controllers/UserAvatarController.cs
@@ -46,34 +46,11 @@ namespace Timeline.Controllers
                 return NotFound(ErrorResponse.UserCommon.NotExist());
             }
 
-            const string IfNonMatchHeaderKey = "If-None-Match";
-
-            var eTagValue = $"\"{await _service.GetAvatarETag(id)}\"";
-            var eTag = new EntityTagHeaderValue(eTagValue);
-
-            if (Request.Headers.TryGetValue(IfNonMatchHeaderKey, out var value))
+            return await DataCacheHelper.GenerateActionResult(this, () => _service.GetAvatarETag(id), async () =>
             {
-                if (!EntityTagHeaderValue.TryParseStrictList(value, out var eTagList))
-                {
-                    _logger.LogInformation(Log.Format(LogGetBadIfNoneMatch,
-                        ("Username", username), ("If-None-Match", value)));
-                    return BadRequest(ErrorResponse.Common.Header.IfNonMatch_BadFormat());
-                }
-
-                if (eTagList.FirstOrDefault(e => e.Equals(eTag)) != null)
-                {
-                    Response.Headers.Add("ETag", eTagValue);
-                    _logger.LogInformation(Log.Format(LogGetReturnNotModify, ("Username", username)));
-                    return StatusCode(StatusCodes.Status304NotModified);
-                }
-            }
-
-            var avatarInfo = await _service.GetAvatar(id);
-            var avatar = avatarInfo.Avatar;
-
-            _logger.LogInformation(Log.Format(LogGetReturnData, ("Username", username)));
-            return File(avatar.Data, avatar.Type, new DateTimeOffset(avatarInfo.LastModified), eTag);
-
+                var avatar = await _service.GetAvatar(id);
+                return avatar.ToCacheableData();
+            });
         }
 
         [HttpPut("users/{username}/avatar")]
