2ffc07756ca7d50533e90fdac37d62a6c33a8a00
crupest <crupest@outlook.com>
2020-05-30 23:16:49 +0800

    Just get everything works!

--- Timeline/ClientApp/src/config.ts
+++ Timeline/ClientApp/src/config.ts
@@ -1,12 +1,1 @@
-export const apiBaseUrl = (function () {
-  if (process.env.TIMELINE_USEDEVAPI) {
-    console.log('process.env.TIMELINE_USEDEVAPI is set, use dev api server!');
-    return 'http://localhost:5000';
-  } else if (process.env.NODE_ENV === 'production') {
-    console.log('Production mode!');
-    return 'https://api.crupest.xyz';
-  } else {
-    console.log('Development mode!');
-    return 'http://localhost:5000';
-  }
-})();
+export const apiBaseUrl = '/api';
