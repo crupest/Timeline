cc62df19e5f8aa216660915f46ff290c8eeab1d0
crupest <crupest@outlook.com>
2020-06-12 18:39:24 +0800

    refactor(backend): Move some string constant from PathProvider to ApplicationConfiguration.

--- Timeline/Configs/ApplicationConfiguration.cs
+++ Timeline/Configs/ApplicationConfiguration.cs
@@ -2,6 +2,9 @@
 {
     public static class ApplicationConfiguration
     {
+        public const string WorkDirKey = "WorkDir";
+        public const string DefaultWorkDir = "/timeline";
+        public const string DatabaseFileName = "timeline.db";
         public const string DisableFrontEndKey = "DisableFrontEnd";
         public const string FrontEndProxyOnlyKey = "FrontEndProxyOnly";
     }
