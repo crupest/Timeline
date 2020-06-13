8f3068beb4b735a3430ecd80bda4162c53d02bc0
crupest <crupest@outlook.com>
2020-06-13 23:50:09 +0800

    feat(back): Add database migration to add unique id for timeline.

--- Timeline/Entities/DatabaseContext.cs
+++ Timeline/Entities/DatabaseContext.cs
@@ -1,4 +1,6 @@
+using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
+using System;
 
 namespace Timeline.Entities
 {
@@ -7,6 +9,15 @@ namespace Timeline.Entities
         public DatabaseContext(DbContextOptions<DatabaseContext> options)
             : base(options)
         {
+            if (Database.IsSqlite())
+            {
+                var connection = (SqliteConnection)Database.GetDbConnection();
+                connection.CreateFunction("timeline_create_guid", () => Guid.NewGuid().ToString());
+            }
+            else
+            {
+                throw new InvalidOperationException(Resources.Entities.ExceptionOnlySqliteSupported);
+            }
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -14,6 +25,7 @@ namespace Timeline.Entities
             modelBuilder.Entity<UserEntity>().Property(e => e.Version).HasDefaultValue(0);
             modelBuilder.Entity<UserEntity>().HasIndex(e => e.Username).IsUnique();
             modelBuilder.Entity<DataEntity>().HasIndex(e => e.Tag).IsUnique();
+            modelBuilder.Entity<TimelineEntity>().Property(e => e.UniqueId).HasDefaultValueSql("timeline_create_guid()");
         }
 
         public DbSet<UserEntity> Users { get; set; } = default!;
---
+++ Timeline/Migrations/20200613135613_AddTimelineUniqueId.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Timeline.Migrations
+{
+    public partial class AddTimelineUniqueId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+@"
+ALTER TABLE timelines RENAME TO timelines_backup;
+
+CREATE TABLE timelines (
+    id INTEGER NOT NULL CONSTRAINT PK_timelines PRIMARY KEY AUTOINCREMENT,
+	unique_id TEXT NOT NULL DEFAULT (timeline_create_guid()),
+    name TEXT NULL,
+    description TEXT NULL,
+    owner INTEGER NOT NULL,
+    visibility INTEGER NOT NULL,
+    create_time TEXT NOT NULL, current_post_local_id INTEGER NOT NULL DEFAULT 0,
+    CONSTRAINT FK_timelines_users_owner FOREIGN KEY (owner) REFERENCES users (id) ON DELETE CASCADE
+);
+
+INSERT INTO timelines (id, name, description, owner, visibility, create_time)
+	SELECT id, name, description, owner, visibility, create_time FROM timelines_backup;
+
+DROP TABLE timelines_backup;
+"
+            );
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "unique_id",
+                table: "timelines");
+        }
+    }
+}
