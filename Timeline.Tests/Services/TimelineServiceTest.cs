96f0b52cd963d72a9a473ac3f0aa83dc7251063c
crupest <crupest@outlook.com>
2020-07-10 16:10:17 +0800

    Add unit tests.

--- Timeline.Tests/Services/TimelineServiceTest.cs
+++ Timeline.Tests/Services/TimelineServiceTest.cs
@@ -118,5 +118,44 @@ namespace Timeline.Tests.Services
             posts.Should().HaveCount(3)
                 .And.Subject.Select(p => (p.Content as TextTimelinePostContent).Text).Should().Equal(postContentList.Skip(1));
         }
+
+        [Theory]
+        [InlineData("@user")]
+        [InlineData("tl")]
+        public async Task GetPosts_IncludeDeleted(string timelineName)
+        {
+            var userId = await _userService.GetUserIdByUsername("user");
+
+            var _ = TimelineHelper.ExtractTimelineName(timelineName, out var isPersonal);
+            if (!isPersonal)
+                await _timelineService.CreateTimeline(timelineName, userId);
+
+            var postContentList = new string[] { "a", "b", "c", "d" };
+
+            foreach (var content in postContentList)
+            {
+                await _timelineService.CreateTextPost(timelineName, userId, content, null);
+            }
+
+            var posts = await _timelineService.GetPosts(timelineName);
+            posts.Should().HaveCount(4);
+            posts.Select(p => p.Deleted).Should().Equal(Enumerable.Repeat(false, posts.Count));
+            posts.Select(p => ((TextTimelinePostContent)p.Content).Text).Should().Equal(postContentList);
+
+            foreach (var id in new long[] { posts[0].Id, posts[2].Id })
+            {
+                await _timelineService.DeletePost(timelineName, id);
+            }
+
+            posts = await _timelineService.GetPosts(timelineName);
+            posts.Should().HaveCount(2);
+            posts.Select(p => p.Deleted).Should().Equal(Enumerable.Repeat(false, posts.Count));
+            posts.Select(p => ((TextTimelinePostContent)p.Content).Text).Should().Equal(new string[] { "b", "d" });
+
+            posts = await _timelineService.GetPosts(timelineName, includeDeleted: true);
+            posts.Should().HaveCount(4);
+            posts.Select(p => p.Deleted).Should().Equal(new bool[] { true, false, true, false });
+            posts.Where(p => !p.Deleted).Select(p => ((TextTimelinePostContent)p.Content).Text).Should().Equal(new string[] { "b", "d" });
+        }
     }
 }
