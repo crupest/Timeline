e69190abb09661caa19fa3905a0d8f3b7e72648b
crupest <crupest@outlook.com>
2020-07-13 20:59:52 +0800

    Move front end to a submodule.

--- Timeline/ClientApp/src/app/data/base64.ts
+++
@@ -1,9 +0,0 @@
-export function base64(blob: Blob): Promise<string> {
-  return new Promise<string>(resolve => {
-    const reader = new FileReader();
-    reader.onload = function() {
-      resolve((reader.result as string).replace(/^data:.+;base64,/, ''));
-    };
-    reader.readAsDataURL(blob);
-  });
-}
