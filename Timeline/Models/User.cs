41be2bcec26ff46829980c563f43c197505f0c8b
crupest <crupest@outlook.com>
2020-08-11 00:39:10 +0800

    User service update time fields of user.

--- Timeline/Models/User.cs
+++ Timeline/Models/User.cs
@@ -1,19 +1,21 @@
-﻿namespace Timeline.Models
+﻿using System;
+
+namespace Timeline.Models
 {
     public class User
     {
         public string? UniqueId { get; set; }
         public string? Username { get; set; }
         public string? Nickname { get; set; }
-
-        #region adminsecret
         public bool? Administrator { get; set; }
-        #endregion adminsecret
 
         #region secret
         public long? Id { get; set; }
         public string? Password { get; set; }
         public long? Version { get; set; }
+        public DateTimeOffset? UsernameChangeTime { get; set; }
+        public DateTimeOffset? CreateTime { get; set; }
+        public DateTimeOffset? LastModified { get; set; }
         #endregion secret
     }
 }
