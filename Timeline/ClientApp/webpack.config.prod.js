2a36e7ce0febae1e04bec2bb8d92983dc32b71ba
crupest <crupest@outlook.com>
2020-08-24 01:20:37 +0800

    Migrate to webpack chain.

--- Timeline/ClientApp/webpack.config.prod.js
+++ Timeline/ClientApp/webpack.config.prod.js
@@ -1,84 +1,36 @@
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const WorkboxPlugin = require('workbox-webpack-plugin');
-const PnpWebpackPlugin = require('pnp-webpack-plugin');
 
-const { commonRules, htmlCommonConfig } = require('./webpack.common');
+const config = require('./webpack.common');
 
-const config = {
-  entry: ['./src/app/index.tsx', './src/app/service-worker.tsx'],
-  mode: 'production',
-  devtool: 'source-map',
-  module: {
-    rules: [
-      ...commonRules,
+config.mode('production');
+
+config
+  .entry('index')
+  .add(path.resolve(__dirname, 'src/app/service-worker.tsx'));
+
+config.devtool('source-map');
+
+config.plugin('clean').use(CleanWebpackPlugin);
+
+config.plugin('copy').use(CopyPlugin, [
+  {
+    patterns: [
       {
-        test: /\.ts(x?)$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'babel-loader',
-          },
-          {
-            loader: 'ts-loader',
-          },
-        ],
-      },
-      {
-        test: /\.js(x?)$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'babel-loader',
-          },
-        ],
+        from: path.resolve(__dirname, 'public/'),
+        to: path.resolve(__dirname, 'dist/'),
       },
     ],
   },
-  resolve: {
-    extensions: ['*', '.js', '.jsx', '.ts', '.tsx'],
-    plugins: [PnpWebpackPlugin],
-  },
-  resolveLoader: {
-    plugins: [PnpWebpackPlugin.moduleLoader(module)],
-  },
-  optimization: {
-    runtimeChunk: 'single',
-    splitChunks: {
-      chunks: 'all',
-      cacheGroups: {
-        vendor: {
-          test: /[\\/]node_modules[\\/]/,
-          name: 'vendors',
-          chunks: 'all',
-        },
-      },
-    },
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist/'),
-    filename: '[name].[hash].js',
-    chunkFilename: '[name].[hash].js',
-    publicPath: '/',
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin(htmlCommonConfig),
-    new CopyPlugin({
-      patterns: [
-        {
-          from: path.resolve(__dirname, 'public/'),
-          to: path.resolve(__dirname, 'dist/'),
-        },
-      ],
-    }),
-    new WorkboxPlugin.InjectManifest({
-      swSrc: './src/sw/sw.ts',
-      maximumFileSizeToCacheInBytes: 15000000,
-    }),
-  ],
-};
+]);
 
-module.exports = config;
+config.plugin('workbox').use(WorkboxPlugin.InjectManifest, [
+  {
+    swSrc: path.resolve(__dirname, 'src/sw/sw.ts'),
+    maximumFileSizeToCacheInBytes: 15000000,
+  },
+]);
+
+module.exports = config.toConfig();
