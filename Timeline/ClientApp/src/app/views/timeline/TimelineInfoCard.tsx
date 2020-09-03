ade9ed64bd06384dfa3ea34f6fca61c58c047680
crupest <crupest@outlook.com>
2020-09-03 23:00:38 +0800

    Extract out info card template.

--- Timeline/ClientApp/src/app/views/timeline/TimelineInfoCard.tsx
+++ Timeline/ClientApp/src/app/views/timeline/TimelineInfoCard.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import clsx from "clsx";
 import { useTranslation } from "react-i18next";
 import { Dropdown, Button } from "react-bootstrap";
 
@@ -8,8 +7,7 @@ import { timelineVisibilityTooltipTranslationMap } from "@/services/timeline";
 
 import BlobImage from "../common/BlobImage";
 import { TimelineCardComponentProps } from "../timeline-common/TimelinePageTemplateUI";
-import CollapseButton from "../timeline-common/CollapseButton";
-import SyncStatusBadge from "../timeline-common/SyncStatusBadge";
+import InfoCardTemplate from "../timeline-common/InfoCardTemplate";
 
 export type OrdinaryTimelineManageItem = "delete";
 
@@ -32,57 +30,55 @@ const TimelineInfoCard: React.FC<TimelineInfoCardProps> = (props) => {
   const avatar = useAvatar(timeline?.owner?.username);
 
   return (
-    <div className={clsx("cru-card p-2 clearfix", props.className)}>
-      <div className="float-right d-flex align-items-center">
-        <SyncStatusBadge status={syncStatus} className="mr-2" />
-        <CollapseButton collapse={collapse} onClick={toggleCollapse} />
-      </div>
-
-      <div style={{ display: collapse ? "none" : "block" }}>
-        <h3 className="text-primary mx-3 d-inline-block align-middle">
-          {timeline.name}
-        </h3>
-        <div className="d-inline-block align-middle">
-          <BlobImage blob={avatar} className="avatar small rounded-circle" />
-          {timeline.owner.nickname}
-          <small className="ml-3 text-secondary">
-            @{timeline.owner.username}
-          </small>
-        </div>
-        <p className="mb-0">{timeline.description}</p>
-        <small className="mt-1 d-block">
-          {t(timelineVisibilityTooltipTranslationMap[timeline.visibility])}
+    <InfoCardTemplate
+      className={props.className}
+      syncStatus={syncStatus}
+      collapse={collapse}
+      toggleCollapse={toggleCollapse}
+    >
+      <h3 className="text-primary mx-3 d-inline-block align-middle">
+        {timeline.name}
+      </h3>
+      <div className="d-inline-block align-middle">
+        <BlobImage blob={avatar} className="avatar small rounded-circle" />
+        {timeline.owner.nickname}
+        <small className="ml-3 text-secondary">
+          @{timeline.owner.username}
         </small>
-        <div className="text-right mt-2">
-          {onManage != null ? (
-            <Dropdown>
-              <Dropdown.Toggle variant="outline-primary">
-                {t("timeline.manage")}
-              </Dropdown.Toggle>
-              <Dropdown.Menu>
-                <Dropdown.Item onClick={() => onManage("property")}>
-                  {t("timeline.manageItem.property")}
-                </Dropdown.Item>
-                <Dropdown.Item onClick={onMember}>
-                  {t("timeline.manageItem.member")}
-                </Dropdown.Item>
-                <Dropdown.Divider />
-                <Dropdown.Item
-                  className="text-danger"
-                  onClick={() => onManage("delete")}
-                >
-                  {t("timeline.manageItem.delete")}
-                </Dropdown.Item>
-              </Dropdown.Menu>
-            </Dropdown>
-          ) : (
-            <Button variant="outline-primary" onClick={onMember}>
-              {t("timeline.memberButton")}
-            </Button>
-          )}
-        </div>
       </div>
-    </div>
+      <p className="mb-0">{timeline.description}</p>
+      <small className="mt-1 d-block">
+        {t(timelineVisibilityTooltipTranslationMap[timeline.visibility])}
+      </small>
+      <div className="text-right mt-2">
+        {onManage != null ? (
+          <Dropdown>
+            <Dropdown.Toggle variant="outline-primary">
+              {t("timeline.manage")}
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+              <Dropdown.Item onClick={() => onManage("property")}>
+                {t("timeline.manageItem.property")}
+              </Dropdown.Item>
+              <Dropdown.Item onClick={onMember}>
+                {t("timeline.manageItem.member")}
+              </Dropdown.Item>
+              <Dropdown.Divider />
+              <Dropdown.Item
+                className="text-danger"
+                onClick={() => onManage("delete")}
+              >
+                {t("timeline.manageItem.delete")}
+              </Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        ) : (
+          <Button variant="outline-primary" onClick={onMember}>
+            {t("timeline.memberButton")}
+          </Button>
+        )}
+      </div>
+    </InfoCardTemplate>
   );
 };
 
--- Timeline/ClientApp/src/app/views/user/UserInfoCard.tsx
+++ Timeline/ClientApp/src/app/views/user/UserInfoCard.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import clsx from "clsx";
 import { useTranslation } from "react-i18next";
 import { Dropdown, Button } from "react-bootstrap";
 
@@ -8,8 +7,7 @@ import { useAvatar } from "@/services/user";
 
 import BlobImage from "../common/BlobImage";
 import { TimelineCardComponentProps } from "../timeline-common/TimelinePageTemplateUI";
-import SyncStatusBadge from "../timeline-common/SyncStatusBadge";
-import CollapseButton from "../timeline-common/CollapseButton";
+import InfoCardTemplate from "../timeline-common/InfoCardTemplate";
 
 export type PersonalTimelineManageItem = "avatar" | "nickname";
 
@@ -31,53 +29,51 @@ const UserInfoCard: React.FC<UserInfoCardProps> = (props) => {
   const avatar = useAvatar(timeline?.owner?.username);
 
   return (
-    <div className={clsx("cru-card p-2 clearfix", props.className)}>
-      <div className="float-right d-flex align-items-center">
-        <SyncStatusBadge status={syncStatus} className="mr-2" />
-        <CollapseButton collapse={collapse} onClick={toggleCollapse} />
-      </div>
-
-      <div style={{ display: collapse ? "none" : "block" }}>
-        <div>
-          <BlobImage blob={avatar} className="avatar" />
-          {timeline.owner.nickname}
-          <small className="ml-3 text-secondary">
-            @{timeline.owner.username}
-          </small>
-        </div>
-        <p className="mb-0">{timeline.description}</p>
-        <small className="mt-1 d-block">
-          {t(timelineVisibilityTooltipTranslationMap[timeline.visibility])}
+    <InfoCardTemplate
+      className={props.className}
+      syncStatus={syncStatus}
+      collapse={collapse}
+      toggleCollapse={toggleCollapse}
+    >
+      <div>
+        <BlobImage blob={avatar} className="avatar" />
+        {timeline.owner.nickname}
+        <small className="ml-3 text-secondary">
+          @{timeline.owner.username}
         </small>
-        <div className="text-right mt-2">
-          {onManage != null ? (
-            <Dropdown>
-              <Dropdown.Toggle variant="outline-primary">
-                {t("timeline.manage")}
-              </Dropdown.Toggle>
-              <Dropdown.Menu>
-                <Dropdown.Item onClick={() => onManage("nickname")}>
-                  {t("timeline.manageItem.nickname")}
-                </Dropdown.Item>
-                <Dropdown.Item onClick={() => onManage("avatar")}>
-                  {t("timeline.manageItem.avatar")}
-                </Dropdown.Item>
-                <Dropdown.Item onClick={() => onManage("property")}>
-                  {t("timeline.manageItem.property")}
-                </Dropdown.Item>
-                <Dropdown.Item onClick={onMember}>
-                  {t("timeline.manageItem.member")}
-                </Dropdown.Item>
-              </Dropdown.Menu>
-            </Dropdown>
-          ) : (
-            <Button variant="outline-primary" onClick={onMember}>
-              {t("timeline.memberButton")}
-            </Button>
-          )}
-        </div>
       </div>
-    </div>
+      <p className="mb-0">{timeline.description}</p>
+      <small className="mt-1 d-block">
+        {t(timelineVisibilityTooltipTranslationMap[timeline.visibility])}
+      </small>
+      <div className="text-right mt-2">
+        {onManage != null ? (
+          <Dropdown>
+            <Dropdown.Toggle variant="outline-primary">
+              {t("timeline.manage")}
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+              <Dropdown.Item onClick={() => onManage("nickname")}>
+                {t("timeline.manageItem.nickname")}
+              </Dropdown.Item>
+              <Dropdown.Item onClick={() => onManage("avatar")}>
+                {t("timeline.manageItem.avatar")}
+              </Dropdown.Item>
+              <Dropdown.Item onClick={() => onManage("property")}>
+                {t("timeline.manageItem.property")}
+              </Dropdown.Item>
+              <Dropdown.Item onClick={onMember}>
+                {t("timeline.manageItem.member")}
+              </Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        ) : (
+          <Button variant="outline-primary" onClick={onMember}>
+            {t("timeline.memberButton")}
+          </Button>
+        )}
+      </div>
+    </InfoCardTemplate>
   );
 };
 
