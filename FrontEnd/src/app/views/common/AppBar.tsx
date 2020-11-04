8f4b972fc06bfbe3f2b1330472c3b704f0759eec
crupest <crupest@outlook.com>
2020-11-04 17:11:18 +0800

    feat: Collapse app bar after select item.

--- FrontEnd/src/app/views/common/AppBar.tsx
+++ FrontEnd/src/app/views/common/AppBar.tsx
@@ -17,27 +17,52 @@ const AppBar: React.FC = (_) => {
 
   const isAdministrator = user && user.administrator;
 
+  const [expand, setExpand] = React.useState<boolean>(false);
+  const collapse = (): void => setExpand(false);
+  const toggleExpand = (): void => setExpand(!expand);
+
   return (
-    <Navbar bg="primary" variant="dark" expand="md" sticky="top">
-      <LinkContainer to="/">
+    <Navbar
+      bg="primary"
+      variant="dark"
+      expand="md"
+      sticky="top"
+      expanded={expand}
+    >
+      <LinkContainer to="/" onClick={collapse}>
         <Navbar.Brand className="d-flex align-items-center">
           <TimelineLogo style={{ height: "1em" }} />
           Timeline
         </Navbar.Brand>
       </LinkContainer>
 
-      <Navbar.Toggle />
+      <Navbar.Toggle onClick={toggleExpand} />
       <Navbar.Collapse>
         <Nav className="mr-auto">
-          <NavLink to="/settings" className="nav-link" activeClassName="active">
+          <NavLink
+            to="/settings"
+            className="nav-link"
+            activeClassName="active"
+            onClick={collapse}
+          >
             {t("nav.settings")}
           </NavLink>
-          <NavLink to="/about" className="nav-link" activeClassName="active">
+          <NavLink
+            to="/about"
+            className="nav-link"
+            activeClassName="active"
+            onClick={collapse}
+          >
             {t("nav.about")}
           </NavLink>
 
           {isAdministrator && (
-            <NavLink to="/admin" className="nav-link" activeClassName="active">
+            <NavLink
+              to="/admin"
+              className="nav-link"
+              activeClassName="active"
+              onClick={collapse}
+            >
               Administration
             </NavLink>
           )}
@@ -46,12 +71,18 @@ const AppBar: React.FC = (_) => {
           {user != null ? (
             <LinkContainer to={`/users/${user.username}`}>
               <BlobImage
-                className="avatar small rounded-circle bg-white cursor-pointer"
+                className="avatar small rounded-circle bg-white cursor-pointer ml-auto"
+                onClick={collapse}
                 blob={avatar}
               />
             </LinkContainer>
           ) : (
-            <NavLink to="/login" className="nav-link" activeClassName="active">
+            <NavLink
+              to="/login"
+              className="nav-link"
+              activeClassName="active"
+              onClick={collapse}
+            >
               {t("nav.login")}
             </NavLink>
           )}
