5321e0a4fac931c4a17f515799bf37948d1cea0e
crupest <crupest@outlook.com>
2020-11-05 17:30:58 +0800

    deploy: Add some deploy script.

---
+++ deploy/upgrade.bash
@@ -0,0 +1,6 @@
+docker pull crupest/timeline:latest
+sudo systemctl stop timeline.service
+docker rm timeline
+docker create -v $HOME/timeline:/timeline -p 5000:80 --name timeline crupest/timeline:latest
+sudo systemctl restart timeline.service
+docker system prune
