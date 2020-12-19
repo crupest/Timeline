f887d95fde97e3a31be605c9d706cc764a170947
crupest <crupest@outlook.com>
2020-12-19 21:42:58 +0800

    ...

--- FrontEnd/src/app/views/home/BoardWithUser.tsx
+++ FrontEnd/src/app/views/home/BoardWithUser.tsx
@@ -12,87 +12,51 @@ import OfflineBoard from "./OfflineBoard";
 const BoardWithUser: React.FC<{ user: AuthUser }> = ({ user }) => {
   const { t } = useTranslation();
 
-  const [ownTimelines, setOwnTimelines] = React.useState<
-    TimelineInfo[] | "offline" | "loading"
-  >("loading");
-  const [joinTimelines, setJoinTimelines] = React.useState<
+  const [relatedTimelines, setRelatedTimelines] = React.useState<
     TimelineInfo[] | "offline" | "loading"
   >("loading");
 
   React.useEffect(() => {
     let subscribe = true;
-    if (ownTimelines === "loading") {
+    if (relatedTimelines === "loading") {
       void getHttpTimelineClient()
-        .listTimeline({ relate: user.username, relateType: "own" })
+        .listTimeline({ relate: user.username })
         .then(
           (timelines) => {
             if (subscribe) {
-              setOwnTimelines(timelines);
+              setRelatedTimelines(timelines);
             }
           },
           () => {
-            setOwnTimelines("offline");
+            setRelatedTimelines("offline");
           }
         );
     }
     return () => {
       subscribe = false;
     };
-  }, [user, ownTimelines]);
-
-  React.useEffect(() => {
-    let subscribe = true;
-    if (joinTimelines === "loading") {
-      void getHttpTimelineClient()
-        .listTimeline({ relate: user.username, relateType: "join" })
-        .then(
-          (timelines) => {
-            if (subscribe) {
-              setJoinTimelines(timelines);
-            }
-          },
-          () => {
-            setJoinTimelines("offline");
-          }
-        );
-    }
-    return () => {
-      subscribe = false;
-    };
-  }, [user, joinTimelines]);
+  }, [user, relatedTimelines]);
 
   return (
     <Row className="my-3 justify-content-center">
-      {ownTimelines === "offline" && joinTimelines === "offline" ? (
+      {relatedTimelines === "offline" ? (
         <Col sm="8" lg="6">
           <OfflineBoard
             onReload={() => {
-              setOwnTimelines("loading");
-              setJoinTimelines("loading");
+              setRelatedTimelines("loading");
             }}
           />
         </Col>
       ) : (
-        <>
-          <Col sm="6" lg="5" className="mb-3 mb-sm-0">
-            <TimelineBoard
-              title={t("home.ownTimeline")}
-              timelines={ownTimelines}
-              onReload={() => {
-                setOwnTimelines("loading");
-              }}
-            />
-          </Col>
-          <Col sm="6" lg="5">
-            <TimelineBoard
-              title={t("home.joinTimeline")}
-              timelines={joinTimelines}
-              onReload={() => {
-                setJoinTimelines("loading");
-              }}
-            />
-          </Col>
-        </>
+        <Col sm="6" lg="5">
+          <TimelineBoard
+            title={t("home.relatedTimeline")}
+            timelines={relatedTimelines}
+            onReload={() => {
+              setRelatedTimelines("loading");
+            }}
+          />
+        </Col>
       )}
     </Row>
   );
