5eb47dabf3239147d158296c6442e98b62747928
crupest <crupest@outlook.com>
2021-02-04 21:33:42 +0800

    database: Upgrade database.

---
+++ BackEnd/Timeline/Migrations/20210204133241_AddColor.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Timeline.Migrations
+{
+    public partial class AddColor : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "color",
+                table: "timelines",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "color",
+                table: "timeline_posts",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "color",
+                table: "timelines");
+
+            migrationBuilder.DropColumn(
+                name: "color",
+                table: "timeline_posts");
+        }
+    }
+}
