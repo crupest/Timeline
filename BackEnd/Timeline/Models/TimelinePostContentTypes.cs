8258f2ffc71c2f7ccea89d1290b37dce29ef4e7a
crupest <crupest@outlook.com>
2021-02-06 16:41:19 +0800

    ...

---
+++ BackEnd/Timeline/Models/TimelinePostContentTypes.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Timeline.Models
+{
+    public static class TimelinePostContentTypes
+    {
+        public static string[] AllTypes { get; } = new string[] { Text, Image };
+        public const string Text = "text";
+        public const string Image = "image";
+    }
+}
