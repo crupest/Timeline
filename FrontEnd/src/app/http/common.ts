446fe790528cb9d8694d857a2cff660bd0febaf8
crupest <crupest@outlook.com>
2021-03-11 10:36:55 +0800

    fix: Fix a bug when base64 string.

--- FrontEnd/src/app/http/common.ts
+++ FrontEnd/src/app/http/common.ts
@@ -65,7 +65,7 @@ export function base64(blob: Blob): Promise<string> {
   return new Promise<string>((resolve) => {
     const reader = new FileReader();
     reader.onload = function () {
-      resolve((reader.result as string).replace(/^data:.+;base64,/, ""));
+      resolve((reader.result as string).replace(/^data:.*;base64,/, ""));
     };
     reader.readAsDataURL(blob);
   });
