f2370781ac566e3adf3b5f887bd49b8613bd874b
crupest <crupest@outlook.com>
2021-04-04 15:59:23 +0800

    ...

--- FrontEnd/src/app/views/timeline-common/TimelineDateLabel.tsx
+++ FrontEnd/src/app/views/timeline-common/TimelineDateLabel.tsx
@@ -8,7 +8,7 @@ export interface TimelineDateItemProps {
 const TimelineDateLabel: React.FC<TimelineDateItemProps> = ({ date }) => {
   return (
     <div className="timeline-date-item">
-      <TimelineLine center={null} />
+      <TimelineLine center="none" />
       <div className="timeline-date-item-badge">
         {date.toLocaleDateString()}
       </div>
--- FrontEnd/src/app/views/timeline-common/TimelineLine.tsx
+++ FrontEnd/src/app/views/timeline-common/TimelineLine.tsx
@@ -4,7 +4,7 @@ import React from "react";
 export interface TimelineLineProps {
   current?: boolean;
   startSegmentLength?: string | number;
-  center: "node" | null;
+  center: "node" | "loading" | "none";
   className?: string;
   style?: React.CSSProperties;
 }
@@ -19,12 +19,22 @@ const TimelineLine: React.FC<TimelineLineProps> = ({
   return (
     <div className={clsx("timeline-line", className)} style={style}>
       <div className="segment start" style={{ height: startSegmentLength }} />
-      {center == "node" ? (
+      {center !== "none" ? (
         <div className="node-container">
           <div className="node"></div>
+          {center === "loading" ? (
+            <svg className="node-loading-edge" viewBox="0 0 100 100">
+              <path
+                d="M 50,10 A 40 40 45 0 1 78.28,21.72"
+                stroke="currentcolor"
+                strokeLinecap="square"
+                strokeWidth="8"
+              />
+            </svg>
+          ) : null}
         </div>
       ) : null}
-      <div className="segment end"></div>
+      {center !== "loading" ? <div className="segment end"></div> : null}
       {current && <div className="segment current-end" />}
     </div>
   );
