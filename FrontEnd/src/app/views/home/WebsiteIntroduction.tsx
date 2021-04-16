d69046007df244ea380ed29e7ca25a87ba9c03d7
crupest <crupest@outlook.com>
2021-04-16 18:18:01 +0800

    ...

--- FrontEnd/src/app/views/home/WebsiteIntroduction.tsx
+++ FrontEnd/src/app/views/home/WebsiteIntroduction.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
+import { Link } from "react-router-dom";
 
 const WebsiteIntroduction: React.FC<{
   className?: string;
@@ -10,18 +11,22 @@ const WebsiteIntroduction: React.FC<{
   if (i18n.language.startsWith("zh")) {
     return (
       <div className={className} style={style}>
-        <h2>欢迎来到时间线！🎉🎉🎉</h2>
+        <h2>
+          欢迎来到<strong>时间线</strong>！🎉🎉🎉
+        </h2>
         <p>
           本网站由无数个独立的时间线构成，每一个时间线都是一个消息列表，类似于一个聊天软件（比如QQ）。
         </p>
         <p>
-          如果你拥有一个账号，登陆后你可以自由地在属于你的时间线中发送内容，支持markdown和上传图片哦！你可以创建一个新的时间线来开启一个新的话题。你也可以设置相关权限，只让一部分人能看到时间线的内容。
+          如果你拥有一个账号，<Link to="/login">登陆</Link>
+          后你可以自由地在属于你的时间线中发送内容，支持markdown和上传图片哦！你可以创建一个新的时间线来开启一个新的话题。你也可以设置相关权限，只让一部分人能看到时间线的内容。
         </p>
         <p>
           如果你没有账号，那么你可以去浏览一下公开的时间线，比如下面这些站长设置的高光时间线。
         </p>
         <p>
-          鉴于这个网站在我的小型服务器上部署，所以没有开放注册。如果你也想把这个服务部署到自己的服务器上，你可以在关于页面找到一些信息。
+          鉴于这个网站在我的小型服务器上部署，所以没有开放注册。如果你也想把这个服务部署到自己的服务器上，你可以在
+          <Link to="/about">关于</Link>页面找到一些信息。
         </p>
         <p>
           <small className="text-secondary">
@@ -33,17 +38,19 @@ const WebsiteIntroduction: React.FC<{
   } else {
     return (
       <div className={className} style={style}>
-        <h2>Welcome to Timeline!🎉🎉🎉</h2>
+        <h2>
+          Welcome to <strong>Timeline</strong>!🎉🎉🎉
+        </h2>
         <p>
           This website consists of many individual timelines. Each timeline is a
           list of messages just like a chat app.
         </p>
         <p>
-          If you do have an account, you can post messages, which supports
-          Markdown and images, in your timelines after logging in. You can also
-          create a new timeline to open a new topic. You can set the permission
-          of a timeline to only allow specified people to see the content of the
-          timeline.
+          If you do have an account, you can <Link to="/login">login</Link> and
+          post messages, which supports Markdown and images, in your timelines.
+          You can also create a new timeline to open a new topic. You can set
+          the permission of a timeline to only allow specified people to see the
+          content of the timeline.
         </p>
         <p>
           If you don&apos;t have an account, you can view some public timelines
@@ -52,7 +59,8 @@ const WebsiteIntroduction: React.FC<{
         <p>
           Since this website is hosted on my tiny server, so account registry is
           not opened. If you want to host this service on your own server, you
-          can find some useful information on about page.
+          can find some useful information on <Link to="/about">about</Link>{" "}
+          page.
         </p>
         <p>
           <small className="text-secondary">
