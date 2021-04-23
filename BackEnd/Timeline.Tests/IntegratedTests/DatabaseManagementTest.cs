67ed852e56696cee15763c9a2b92acf36e7409c0
crupest <crupest@outlook.com>
2021-04-23 17:36:50 +0800

    test: Add database backup test.

---
+++ BackEnd/Timeline.Tests/IntegratedTests/DatabaseManagementTest.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using System.IO;
+using Timeline.Services;
+using Xunit;
+
+namespace Timeline.Tests.IntegratedTests
+{
+    public class DatabaseManagementTest : IntegratedTestBase
+    {
+        [Fact]
+        public void Backup_Should_Work()
+        {
+            var pathProvider = TestApp.Host.Services.GetRequiredService<IPathProvider>();
+            var backupDir = pathProvider.GetDatabaseBackupDirectory();
+            Directory.GetFiles(backupDir).Should().NotBeEmpty();
+        }
+    }
+}
