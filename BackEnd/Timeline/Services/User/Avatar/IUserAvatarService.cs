851ecac931c81f77103f930459ee3e7279134cf3
crupest <crupest@outlook.com>
2021-04-27 19:37:11 +0800

    refactor: ...

---
+++ BackEnd/Timeline/Services/User/Avatar/IUserAvatarService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks;
+using Timeline.Helpers.Cache;
+using Timeline.Models;
+using Timeline.Services.Imaging;
+
+namespace Timeline.Services.User.Avatar
+{
+    public interface IUserAvatarService
+    {
+        /// <summary>
+        /// Get avatar digest of a user.
+        /// </summary>
+        /// <param name="userId">User id.</param>
+        /// <returns>The avatar digest.</returns>
+        /// <exception cref="UserNotExistException">Thrown when user does not exist.</exception>
+        Task<ICacheableDataDigest> GetAvatarDigest(long userId);
+
+        /// <summary>
+        /// Get avatar of a user. If the user has no avatar set, a default one is returned.
+        /// </summary>
+        /// <param name="userId">User id.</param>
+        /// <returns>The avatar.</returns>
+        /// <exception cref="UserNotExistException">Thrown when user does not exist.</exception>
+        Task<ByteData> GetAvatar(long userId);
+
+        /// <summary>
+        /// Set avatar for a user.
+        /// </summary>
+        /// <param name="userId">User id.</param>
+        /// <param name="avatar">The new avatar data.</param>
+        /// <returns>The digest of the avatar.</returns>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="avatar"/> is null.</exception>
+        /// <exception cref="UserNotExistException">Thrown when user does not exist.</exception>
+        /// <exception cref="ImageException">Thrown if avatar is of bad format.</exception>
+        Task<ICacheableDataDigest> SetAvatar(long userId, ByteData avatar);
+
+        /// <summary>
+        /// Remove avatar of a user.
+        /// </summary>
+        /// <param name="userId">User id.</param>
+        /// <exception cref="UserNotExistException">Thrown when user does not exist.</exception>
+        Task DeleteAvatar(long userId);
+    }
+}
--- BackEnd/Timeline/Services/User/Avatar/UserAvatarService.cs
+++ BackEnd/Timeline/Services/User/Avatar/UserAvatarService.cs
@@ -1,10 +1,6 @@
-﻿using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
-using SixLabors.ImageSharp;
 using System;
-using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Timeline.Entities;
@@ -13,114 +9,15 @@ using Timeline.Models;
 using Timeline.Services.Data;
 using Timeline.Services.Imaging;
 
-namespace Timeline.Services.User
+namespace Timeline.Services.User.Avatar
 {
-    /// <summary>
-    /// Provider for default user avatar.
-    /// </summary>
-    /// <remarks>
-    /// Mainly for unit tests.
-    /// </remarks>
-    public interface IDefaultUserAvatarProvider
-    {
-        /// <summary>
-        /// Get the digest of default avatar.
-        /// </summary>
-        /// <returns>The digest.</returns>
-        Task<ICacheableDataDigest> GetDefaultAvatarDigest();
-
-        /// <summary>
-        /// Get the default avatar.
-        /// </summary>
-        /// <returns>The avatar.</returns>
-        Task<ByteData> GetDefaultAvatar();
-    }
-
-    public interface IUserAvatarService
-    {
-        /// <summary>
-        /// Get avatar digest of a user.
-        /// </summary>
-        /// <param name="userId">User id.</param>
-        /// <returns>The avatar digest.</returns>
-        /// <exception cref="UserNotExistException">Thrown when user does not exist.</exception>
-        Task<ICacheableDataDigest> GetAvatarDigest(long userId);
-
-        /// <summary>
-        /// Get avatar of a user. If the user has no avatar set, a default one is returned.
-        /// </summary>
-        /// <param name="userId">User id.</param>
-        /// <returns>The avatar.</returns>
-        /// <exception cref="UserNotExistException">Thrown when user does not exist.</exception>
-        Task<ByteData> GetAvatar(long userId);
-
-        /// <summary>
-        /// Set avatar for a user.
-        /// </summary>
-        /// <param name="userId">User id.</param>
-        /// <param name="avatar">The new avatar data.</param>
-        /// <returns>The digest of the avatar.</returns>
-        /// <exception cref="ArgumentNullException">Thrown if <paramref name="avatar"/> is null.</exception>
-        /// <exception cref="UserNotExistException">Thrown when user does not exist.</exception>
-        /// <exception cref="ImageException">Thrown if avatar is of bad format.</exception>
-        Task<ICacheableDataDigest> SetAvatar(long userId, ByteData avatar);
-
-        /// <summary>
-        /// Remove avatar of a user.
-        /// </summary>
-        /// <param name="userId">User id.</param>
-        /// <exception cref="UserNotExistException">Thrown when user does not exist.</exception>
-        Task DeleteAvatar(long userId);
-    }
-
-    // TODO! : Make this configurable.
-    public class DefaultUserAvatarProvider : IDefaultUserAvatarProvider
-    {
-        private readonly IETagGenerator _eTagGenerator;
-
-        private readonly string _avatarPath;
-
-        private CacheableDataDigest? _cacheDigest;
-        private ByteData? _cacheData;
-
-        public DefaultUserAvatarProvider(IWebHostEnvironment environment, IETagGenerator eTagGenerator)
-        {
-            _avatarPath = Path.Combine(environment.ContentRootPath, "default-avatar.png");
-            _eTagGenerator = eTagGenerator;
-        }
-
-        private async Task CheckAndInit()
-        {
-            var path = _avatarPath;
-            if (_cacheData == null || File.GetLastWriteTime(path) > _cacheDigest!.LastModified)
-            {
-                var data = await File.ReadAllBytesAsync(path);
-                _cacheDigest = new CacheableDataDigest(await _eTagGenerator.GenerateETagAsync(data), File.GetLastWriteTime(path));
-                Image.Identify(data, out var format);
-                _cacheData = new ByteData(data, format.DefaultMimeType);
-            }
-        }
-
-        public async Task<ICacheableDataDigest> GetDefaultAvatarDigest()
-        {
-            await CheckAndInit();
-            return _cacheDigest!;
-        }
-
-        public async Task<ByteData> GetDefaultAvatar()
-        {
-            await CheckAndInit();
-            return _cacheData!;
-        }
-    }
-
     public class UserAvatarService : IUserAvatarService
     {
         private readonly ILogger<UserAvatarService> _logger;
         private readonly DatabaseContext _database;
         private readonly IBasicUserService _basicUserService;
         private readonly IDefaultUserAvatarProvider _defaultUserAvatarProvider;
-        private readonly IImageService _imageValidator;
+        private readonly IImageService _imageService;
         private readonly IDataManager _dataManager;
         private readonly IClock _clock;
 
@@ -137,7 +34,7 @@ namespace Timeline.Services.User
             _database = database;
             _basicUserService = basicUserService;
             _defaultUserAvatarProvider = defaultUserAvatarProvider;
-            _imageValidator = imageValidator;
+            _imageService = imageValidator;
             _dataManager = dataManager;
             _clock = clock;
         }
@@ -179,7 +76,7 @@ namespace Timeline.Services.User
 
             if (entity.Type is null)
             {
-                Image.Identify(data, out var format);
+                var format = await _imageService.DetectFormatAsync(data);
                 entity.Type = format.DefaultMimeType;
                 await _database.SaveChangesAsync();
             }
@@ -192,7 +89,7 @@ namespace Timeline.Services.User
             if (avatar is null)
                 throw new ArgumentNullException(nameof(avatar));
 
-            await _imageValidator.ValidateAsync(avatar.Data, avatar.ContentType, true);
+            await _imageService.ValidateAsync(avatar.Data, avatar.ContentType, true);
 
             await _basicUserService.ThrowIfUserNotExist(userId);
 
@@ -252,15 +149,6 @@ namespace Timeline.Services.User
             await _database.SaveChangesAsync();
 
             await transaction.CommitAsync();
-        }
-    }
-
-    public static class UserAvatarServiceCollectionExtensions
-    {
-        public static void AddUserAvatarService(this IServiceCollection services)
-        {
-            services.AddScoped<IUserAvatarService, UserAvatarService>();
-            services.AddScoped<IDefaultUserAvatarProvider, DefaultUserAvatarProvider>();
         }
     }
 }
