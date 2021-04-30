5eaf5c4e49dd87417886e20f1324b2f70396fa55
crupest <crupest@outlook.com>
2021-04-30 18:15:05 +0800

    chore: Remove unused codes.

--- BackEnd/Timeline/GlobalSuppressions.cs
+++ BackEnd/Timeline/GlobalSuppressions.cs
@@ -7,7 +7,4 @@ using System.Diagnostics.CodeAnalysis;
 
 [assembly: SuppressMessage("Reliability", "CA2007:Consider calling ConfigureAwait on the awaited task", Justification = "This is not a UI application.")]
 [assembly: SuppressMessage("Design", "CA1034:Nested types should not be visible", Justification = "This is not bad.")]
-[assembly: SuppressMessage("Naming", "CA1707:Identifiers should not contain underscores", Justification = "Generated error response identifiers.", Scope = "type", Target = "~T:Timeline.Models.Http.ErrorResponse")]
-[assembly: SuppressMessage("Naming", "CA1724:Type names should not match namespaces", Justification = "Generated error response identifiers.", Scope = "type", Target = "~T:Timeline.Models.Http.ErrorResponse")]
-[assembly: SuppressMessage("Globalization", "CA1305:Specify IFormatProvider", Justification = "Generated error response.", Scope = "type", Target = "~T:Timeline.Models.Http.ErrorResponse")]
 [assembly: SuppressMessage("Design", "CA1062:Validate arguments of public methods", Justification = "Redundant")]
