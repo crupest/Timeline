1ae2800e799bc0c714b92ef4ce61beafc8350321
crupest <crupest@outlook.com>
2021-04-30 17:00:45 +0800

    refactor: ...

---
+++ BackEnd/Timeline/Models/Http/CommonDeleteResponse.cs
@@ -0,0 +1,54 @@
+﻿namespace Timeline.Models.Http
+{
+    /// <summary>
+    /// Common response for delete method.
+    /// </summary>
+    public class CommonDeleteResponse : CommonDataResponse<CommonDeleteResponse.ResponseData>
+    {
+        /// <summary></summary>
+        public class ResponseData
+        {
+            /// <summary></summary>
+            public ResponseData() { }
+
+            /// <summary></summary>
+            public ResponseData(bool delete)
+            {
+                Delete = delete;
+            }
+
+            /// <summary>
+            /// True if the entry is deleted. False if the entry does not exist.
+            /// </summary>
+            public bool Delete { get; set; }
+        }
+
+        /// <summary></summary>
+        public CommonDeleteResponse()
+        {
+
+        }
+
+        /// <summary></summary>
+        public CommonDeleteResponse(int code, string message, bool delete)
+            : base(code, message, new ResponseData(delete))
+        {
+
+        }
+
+        internal static CommonDeleteResponse Create(bool delete)
+        {
+            return delete ? Delete() : NotExist();
+        }
+
+        internal static CommonDeleteResponse Delete()
+        {
+            return new CommonDeleteResponse(0, Resource.MessageDeleteDelete, true);
+        }
+
+        internal static CommonDeleteResponse NotExist()
+        {
+            return new CommonDeleteResponse(0, Resource.MessageDeleteNotExist, false);
+        }
+    }
+}
---
+++ BackEnd/Timeline/Models/Http/CommonPutResponse.cs
@@ -0,0 +1,43 @@
+﻿namespace Timeline.Models.Http
+{
+    public class CommonPutResponse : CommonDataResponse<CommonPutResponse.ResponseData>
+    {
+        public class ResponseData
+        {
+            public ResponseData() { }
+
+            public ResponseData(bool create)
+            {
+                Create = create;
+            }
+
+            public bool Create { get; set; }
+        }
+
+        public CommonPutResponse()
+        {
+
+        }
+
+        public CommonPutResponse(int code, string message, bool create)
+            : base(code, message, new ResponseData(create))
+        {
+
+        }
+
+        internal static CommonPutResponse Create(bool create)
+        {
+            return create ? Create() : Modify();
+        }
+
+        internal static CommonPutResponse Create()
+        {
+            return new CommonPutResponse(0, Resource.MessagePutCreate, true);
+        }
+
+        internal static CommonPutResponse Modify()
+        {
+            return new CommonPutResponse(0, Resource.MessagePutModify, false);
+        }
+    }
+}
