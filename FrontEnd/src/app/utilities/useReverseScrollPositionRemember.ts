f28bb526eeed0994b3bb986c144bac0be5ce0e04
crupest <crupest@outlook.com>
2021-05-05 18:28:36 +0800

    fix: A very ugly fix of scroll to top problem.

--- FrontEnd/src/app/utilities/useReverseScrollPositionRemember.ts
+++ FrontEnd/src/app/utilities/useReverseScrollPositionRemember.ts
@@ -1,39 +1,47 @@
 import React from "react";
 
 let on = false;
+let recordDisabled = false;
+
+function getScrollPosition(): number {
+  if (document.documentElement.scrollHeight <= window.innerHeight) {
+    return 0;
+  } else {
+    return (
+      document.documentElement.scrollHeight -
+      document.documentElement.scrollTop -
+      window.innerHeight
+    );
+  }
+}
+
+let scrollPosition = getScrollPosition();
+
+function scrollToRecordPosition(): void {
+  if (document.documentElement.scrollHeight <= window.innerHeight) return;
+  document.documentElement.scrollTop =
+    document.documentElement.scrollHeight - window.innerHeight - scrollPosition;
+}
+
+const scrollListener = (): void => {
+  if (recordDisabled) return;
+  scrollPosition = getScrollPosition();
+};
+
+const resizeObserver = new ResizeObserver(() => {
+  scrollToRecordPosition();
+});
+
+export function setRecordDisabled(disabled: boolean): void {
+  recordDisabled = disabled;
+  if (!disabled) scrollToRecordPosition();
+}
 
 export default function useReverseScrollPositionRemember(): void {
   React.useEffect(() => {
     if (on) return;
     on = true;
-
-    function getScrollPosition(): number {
-      if (document.documentElement.scrollHeight <= window.innerHeight) {
-        return 0;
-      } else {
-        return (
-          document.documentElement.scrollHeight -
-          document.documentElement.scrollTop -
-          window.innerHeight
-        );
-      }
-    }
-
-    let scrollPosition = getScrollPosition();
-    const scrollListener = (): void => {
-      scrollPosition = getScrollPosition();
-    };
-
     window.addEventListener("scroll", scrollListener);
-
-    const resizeObserver = new ResizeObserver(() => {
-      if (document.documentElement.scrollHeight <= window.innerHeight) return;
-      document.documentElement.scrollTop =
-        document.documentElement.scrollHeight -
-        window.innerHeight -
-        scrollPosition;
-    });
-
     resizeObserver.observe(document.documentElement);
 
     return () => {
