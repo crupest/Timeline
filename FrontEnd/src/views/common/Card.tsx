1f61176f305a0226cfe928298cb5a30c03c5f3e4
crupest <crupest@outlook.com>
2021-06-22 22:47:21 +0800

    fix: Fix timeline card zindex problem.

--- FrontEnd/src/views/common/Card.tsx
+++ FrontEnd/src/views/common/Card.tsx
@@ -7,11 +7,16 @@ function _Card(
   {
     className,
     children,
+    ...otherProps
   }: React.PropsWithChildren<React.HTMLAttributes<HTMLDivElement>>,
   ref: React.ForwardedRef<HTMLDivElement>
 ): React.ReactElement | null {
   return (
-    <div ref={ref} className={classNames("cru-card", className)}>
+    <div
+      ref={ref}
+      className={classNames("cru-card", className)}
+      {...otherProps}
+    >
       {children}
     </div>
   );
