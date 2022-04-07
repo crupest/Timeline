9e559fc918d4b4485ef589184348686927ebd63c
crupest <crupest@outlook.com>
2022-04-07 22:24:59 +0800

    ...

---
+++ BackEnd/Timeline/Controllers/TimelineV2Controller.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Timeline.Entities;
+using Timeline.Models.Http;
+using Timeline.Services.Mapper;
+using Timeline.Services.Timeline;
+
+namespace Timeline.Controllers
+{
+    [ApiController]
+    [Route("v2/timelines")]
+    public class TimelineV2Controller : MyControllerBase
+    {
+        private ITimelineService _timelineService;
+        private TimelineMapper _timelineMapper;
+
+        public TimelineV2Controller(ITimelineService timelineService, TimelineMapper timelineMapper)
+        {
+            _timelineService = timelineService;
+            _timelineMapper = timelineMapper;
+        }
+
+        [HttpGet("{owner}/{timeline}")]
+        public async Task<ActionResult<HttpTimeline>> Get([FromRoute] string owner, [FromRoute] string timeline)
+        {
+            var timelineId = await _timelineService.GetTimelineIdAsync(owner, timeline);
+            var t = await _timelineService.GetTimelineAsync(timelineId);
+            return await _timelineMapper.MapAsync(t, Url, User);
+        }
+    }
+}
+
