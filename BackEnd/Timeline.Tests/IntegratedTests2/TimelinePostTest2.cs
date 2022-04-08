703e80cbb2e46ea0e5f0221dc88ee92190f90fd9
crupest <crupest@outlook.com>
2022-04-08 22:50:38 +0800

    ...

--- BackEnd/Timeline.Tests/IntegratedTests2/TimelinePostTest2.cs
+++ BackEnd/Timeline.Tests/IntegratedTests2/TimelinePostTest2.cs
@@ -89,7 +89,7 @@ namespace Timeline.Tests.IntegratedTests2
         public async Task PostForbid()
         {
             await CreateUserAsync("user2", "user2pw");
-            var client = CreateClientWithToken(await CreateTokenWithCredentialAsync("user2", "user2pw"));
+            using var client = CreateClientWithToken(await CreateTokenWithCredentialAsync("user2", "user2pw"));
             await client.TestJsonSendAsync(HttpMethod.Post, "v2/timelines/user/hello/posts", new HttpTimelinePostCreateRequest
             {
                 DataList = new List<HttpTimelinePostCreateRequestData>
@@ -117,6 +117,77 @@ namespace Timeline.Tests.IntegratedTests2
                         Data = Convert.ToBase64String(Encoding.UTF8.GetBytes("hello3"))
                     }
                 }
+            }, expectedStatusCode: HttpStatusCode.NotFound);
+        }
+
+        [Fact]
+        public async Task PostDeleteTest()
+        {
+            using var client = CreateClientAsUser();
+            await client.TestSendAsync(HttpMethod.Delete, "v2/timelines/user/hello/posts/1", expectedStatusCode: HttpStatusCode.NoContent);
+        }
+
+        [Fact]
+        public async Task PostDeleteNotLogin()
+        {
+            using var client = CreateDefaultClient();
+            await client.TestSendAsync(HttpMethod.Delete, "v2/timelines/user/hello/posts/1", expectedStatusCode: HttpStatusCode.Unauthorized);
+        }
+
+        [Fact]
+        public async Task PostDeleteForbid()
+        {
+            await CreateUserAsync("user2", "user2pw");
+            using var client = CreateClientWithToken(await CreateTokenWithCredentialAsync("user2", "user2pw"));
+            await client.TestSendAsync(HttpMethod.Delete, "v2/timelines/user/hello/posts/1", expectedStatusCode: HttpStatusCode.Forbidden);
+        }
+
+        [Fact]
+        public async Task PostDeleteNotExist()
+        {
+            using var client = CreateClientAsUser();
+            await client.TestSendAsync(HttpMethod.Delete, "v2/timelines/user/hello/posts/4", expectedStatusCode: HttpStatusCode.NotFound);
+            await client.TestSendAsync(HttpMethod.Delete, "v2/timelines/user/notexist/posts/1", expectedStatusCode: HttpStatusCode.NotFound);
+        }
+
+        [Fact]
+        public async Task PostPatchTest()
+        {
+            using var client = CreateClientAsUser();
+            await client.TestJsonSendAsync(HttpMethod.Patch, "v2/timelines/user/hello/posts/1", new HttpTimelinePostPatchRequest
+            {
+                Color = "#FFFFFF"
+            });
+        }
+
+        [Fact]
+        public async Task PostPatchNotLogin()
+        {
+            using var client = CreateDefaultClient();
+            await client.TestJsonSendAsync(HttpMethod.Patch, "v2/timelines/user/hello/posts/1", new HttpTimelinePostPatchRequest
+            {
+                Color = "#FFFFFF"
+            }, expectedStatusCode: HttpStatusCode.Unauthorized);
+        }
+
+        [Fact]
+        public async Task PostPatchForbid()
+        {
+            await CreateUserAsync("user2", "user2pw");
+            using var client = CreateClientWithToken(await CreateTokenWithCredentialAsync("user2", "user2pw"));
+            await client.TestJsonSendAsync(HttpMethod.Patch, "v2/timelines/user/hello/posts/1", new HttpTimelinePostPatchRequest
+            {
+                Color = "#FFFFFF"
+            }, expectedStatusCode: HttpStatusCode.Forbidden);
+        }
+
+        [Fact]
+        public async Task PostPatchNotExist()
+        {
+            using var client = CreateClientAsUser();
+            await client.TestJsonSendAsync(HttpMethod.Patch, "v2/timelines/user/hello/posts/4", new HttpTimelinePostPatchRequest
+            {
+                Color = "#FFFFFF"
             }, expectedStatusCode: HttpStatusCode.NotFound);
         }
     }
