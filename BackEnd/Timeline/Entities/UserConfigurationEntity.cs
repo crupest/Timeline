9aaca475708de11f173d9f60130bc90457d4bf1b
crupest <crupest@outlook.com>
2022-04-10 21:51:48 +0800

    ...

---
+++ BackEnd/Timeline/Entities/UserConfigurationEntity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Timeline.Models;
+
+namespace Timeline.Entities
+{
+    [Table("user_config")]
+    public class UserConfigurationEntity
+    {
+        [Column("id"), Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }
+
+        [Column("user_id")]
+        public long UserId { get; set; }
+
+        [ForeignKey(nameof(UserId))]
+        public UserEntity User { get; set; } = default!;
+
+        [Column("bookmark_visibility")]
+        public TimelineVisibility BookmarkVisibility { get; set; }
+    }
+}
---
+++ BackEnd/Timeline/Migrations/20220410135026_AddUserConfig.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Timeline.Migrations
+{
+    public partial class AddUserConfig : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "user_config",
+                columns: table => new
+                {
+                    id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    user_id = table.Column<long>(type: "INTEGER", nullable: false),
+                    bookmark_visibility = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_user_config", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_user_config_users_user_id",
+                        column: x => x.user_id,
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_user_config_user_id",
+                table: "user_config",
+                column: "user_id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "user_config");
+        }
+    }
+}
