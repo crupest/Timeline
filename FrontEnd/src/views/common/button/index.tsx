43891d7be318db72fcd7a0ac4b1a8d05bf7b3ce3
crupest <crupest@outlook.com>
2022-04-30 23:04:47 +0800

    ...

---
+++ FrontEnd/src/views/common/button/index.tsx
@@ -0,0 +1,6 @@
+import Button from "./Button";
+import FlatButton from "./FlatButton";
+import IconButton from "./IconButton";
+import LoadingButton from "./LoadingButton";
+
+export { Button, FlatButton, IconButton, LoadingButton };
