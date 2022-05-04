a90f1a5041cf4a622f41f3b2680052e3a4d5ba11
crupest <crupest@outlook.com>
2022-05-04 11:35:47 +0800

    ...

--- FrontEnd/src/index.tsx
+++ FrontEnd/src/index.tsx
@@ -17,4 +17,8 @@ import App from "./App";
 const container = document.getElementById("app");
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const root = createRoot(container!);
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
--- FrontEnd/src/views/common/button/FlatButton.tsx
+++ FrontEnd/src/views/common/button/FlatButton.tsx
@@ -1,16 +1,39 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
+import classNames from "classnames";
 
-import { CommonButtonProps } from "./common";
-import Button from "./Button";
+import { convertI18nText, I18nText } from "@/common";
+import { PaletteColorType } from "@/palette";
 
 import "./FlatButton.css";
 
 function _FlatButton(
-  props: CommonButtonProps,
+  props: {
+    color?: PaletteColorType;
+    text?: I18nText;
+  } & React.ComponentPropsWithoutRef<"button">,
   ref: React.ForwardedRef<HTMLButtonElement>
 ): React.ReactElement | null {
+  const { t } = useTranslation();
+
+  const { color, text, className, children, ...otherProps } = props;
+
+  if (text != null && children != null) {
+    console.warn("You can't set both text and children props.");
+  }
+
   return (
-    <Button ref={ref} customButtonClassName="cru-flat-button" {...props} />
+    <button
+      ref={ref}
+      className={classNames(
+        "cru-" + (color ?? "primary"),
+        "cru-flat-button",
+        className
+      )}
+      {...otherProps}
+    >
+      {text != null ? convertI18nText(text, t) : children}
+    </button>
   );
 }
 
