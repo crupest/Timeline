f8acad20ddb9f5de156d81b54233571b8efba7f9
crupest <crupest@outlook.com>
2023-08-30 00:58:39 +0800

    ...

--- FrontEnd/src/components/Icon.tsx
+++ FrontEnd/src/components/Icon.tsx
@@ -7,20 +7,18 @@ import "./Icon.css";
 
 interface IconButtonProps extends ComponentPropsWithoutRef<"i"> {
   icon: string;
-  color?: ThemeColor | "on-surface";
+  color?: ThemeColor;
   size?: string | number;
 }
 
 export default function Icon(props: IconButtonProps) {
   const { icon, color, size, style, className, ...otherProps } = props;
 
-  const colorName = color === "on-surface" ? "surface-on" : color;
-
   return (
     <i
       style={size != null ? { ...style, fontSize: size } : style}
       className={classNames(
-        colorName && `cru-${colorName}`,
+        `cru-theme-${color ?? "primary"}`,
         `bi-${icon} cru-icon`,
         className,
       )}
