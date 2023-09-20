4165ca210d9660a67b16089c31b7f65b8b09e019
crupest <crupest@outlook.com>
2023-09-20 19:54:18 +0800

    ...

--- FrontEnd/src/pages/home/index.tsx
+++ FrontEnd/src/pages/home/index.tsx
@@ -2,6 +2,11 @@ import "./index.css";
 
 export default function HomePage() {
   return (
-    <div className="home-page">Be patient! I&apos;m working on this...</div>
+    <>
+      <div className="home-page">Be patient! I&apos;m working on this...</div>
+      <div className="home-page-2">
+        Have a look at <a href="/crupest">here</a>!
+      </div>
+    </>
   );
 }
