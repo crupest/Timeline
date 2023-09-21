709904c7bbfcf2e6527cf0da43bf2bcbb1406744
crupest <crupest@outlook.com>
2023-09-21 17:45:06 +0800

    ...

--- FrontEnd/src/components/dialog/Dialog.tsx
+++ FrontEnd/src/components/dialog/Dialog.tsx
@@ -4,6 +4,8 @@ import { CSSTransition } from "react-transition-group";
 
 import { ThemeColor } from "../common";
 
+import InPortal from "../InPortal";
+
 import "./Dialog.css";
 
 interface DialogProps {
@@ -25,35 +27,37 @@ export default function Dialog({
   const lastPointerDownIdRef = useRef<number | null>(null);
 
   return (
-    <CSSTransition
-      nodeRef={nodeRef}
-      mountOnEnter
-      unmountOnExit
-      in={open}
-      timeout={300}
-      classNames="cru-dialog"
-    >
-      <div
-        ref={nodeRef}
-        className={classNames(
-          `cru-theme-${color ?? "primary"}`,
-          "cru-dialog-overlay",
-        )}
+    <InPortal>
+      <CSSTransition
+        nodeRef={nodeRef}
+        mountOnEnter
+        unmountOnExit
+        in={open}
+        timeout={300}
+        classNames="cru-dialog"
       >
         <div
-          className="cru-dialog-background"
-          onPointerDown={(e) => {
-            lastPointerDownIdRef.current = e.pointerId;
-          }}
-          onPointerUp={(e) => {
-            if (lastPointerDownIdRef.current === e.pointerId) {
-              if (!disableCloseOnClickOnOverlay) onClose();
-            }
-            lastPointerDownIdRef.current = null;
-          }}
-        />
-        <div className="cru-dialog-container">{children}</div>
-      </div>
-    </CSSTransition>
+          ref={nodeRef}
+          className={classNames(
+            `cru-theme-${color ?? "primary"}`,
+            "cru-dialog-overlay",
+          )}
+        >
+          <div
+            className="cru-dialog-background"
+            onPointerDown={(e) => {
+              lastPointerDownIdRef.current = e.pointerId;
+            }}
+            onPointerUp={(e) => {
+              if (lastPointerDownIdRef.current === e.pointerId) {
+                if (!disableCloseOnClickOnOverlay) onClose();
+              }
+              lastPointerDownIdRef.current = null;
+            }}
+          />
+          <div className="cru-dialog-container">{children}</div>
+        </div>
+      </CSSTransition>
+    </InPortal>
   );
 }
